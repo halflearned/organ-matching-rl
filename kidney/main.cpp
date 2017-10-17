5348e1b49a564ec127a2ec63cdf10618c8cc7405
halflearned <halflearned@gmail.com>
2017-10-17 12:07:42 -0400

    Initial Commit
    Former-commit-id: 611876463187a07449f7881393e20885a6950e18

---
+++ kidney/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  kidney
+//
+//  Created by Vitor Baisi Hadad on 10/17/17.
+//  Copyright © 2017 halflearned. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
